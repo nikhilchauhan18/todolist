e798b5906c6874ffa3b1229782eaebec5621acb6
Nikhil Chauhan <nikhilchauhantr@gmail.com>
2021-10-21 16:22:20 +0530

    Build functionality

--- app.js
+++ app.js
@@ -3,21 +3,28 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+let items = [];
+
 app.set("view engine", "ejs");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.get("/",function(req,res) {
-  var curdate = new Date();
-  var day = "";
-
-  if(curdate == 0 || curdate == 6){
-    day = "weekend";
-  }else{
-    day = "weekday";
+  let options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long'
   }
+  let curdate = new Date();
+  let day = curdate.toLocaleDateString("en-US",options);
 
-  res.render("list",{day : day});
-
+  res.render("list",{day : day, newitem: items});
 });
+
+app.post("/",function(req,res) {
+  let item = req.body.item;
+  items.push(item);
+  res.redirect("/");
+})
 
 app.listen(3000,function() {
   console.log("running");
